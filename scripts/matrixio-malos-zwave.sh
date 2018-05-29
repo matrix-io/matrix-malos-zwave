163c2ca936fa0094d1069a629d6408c1a5082e76
Kevin Patino <kevindpatino@gmail.com>
2018-05-29 11:19:07 -0500

    Fix initial service

--- scripts/matrixio-malos-zwave.sh
+++ scripts/matrixio-malos-zwave.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-DEVICES=$(avahi-browse _z-wave._udp -r -t -p | grep Controller| cut -d ";" -f8)
-
+DEVICE=$(avahi-browse _z-wave._udp -t | grep Controller | grep Static | cut -d " " -f 4-6 | head -1)
 
 /usr/bin/malos_zwave  -server "${DEVICE}" \
                       -xml /etc/zwave/ZWave_custom_cmd_classes.xml
